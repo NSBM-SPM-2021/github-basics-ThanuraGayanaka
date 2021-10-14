2878221662a8e25e7987b030a0f52fddb04a8182
Thanura gayanaka <57209475+ThanuraGayanaka@users.noreply.github.com>
2021-10-15 01:36:56 +0530

    adding fetch data method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Tasks from './Components/Tasks';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  }, [])
+
+//Fetch Tasks
+const fetchTasks = async () => {
+  const res = await fetch("http://localhost:5000/tasks");
+  const data = await res.json();
+
+  return data;
+}
+
+//Fetch Task
+
+
+//Add Task
+
+
+//Toggle Reminder
+
+
   return (
-    <div className="App">
+    <Router>
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
       
+      <Route path='/' exact render={(props) => (
+        <>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} />) : ("Nothing to show")}
+        </>
+      )} />
     </div>
+    </Router>
+    
   );
 }
 
