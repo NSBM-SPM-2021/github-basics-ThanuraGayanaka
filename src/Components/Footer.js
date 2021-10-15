0347eb75b4faeb4877530818ea70f9a98ab859b8
Thanura gayanaka <57209475+ThanuraGayanaka@users.noreply.github.com>
2021-10-15 12:00:54 +0530

    added missed directory

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+       <footer>
+           <p>Copyright &copy; 2021</p>
+           <Link to="/about">About Us</Link>
+       </footer>
+    )
+}
+
+export default Footer
